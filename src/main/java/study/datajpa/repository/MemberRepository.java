7fa5f311f6c89128f6517b471ae3c7d6d3957312
taz-dev <tazdev@naver.com>
2021-10-28 20:53:59 +0900

    feat: 쿼리 메소드 기능
    1. @Query -> 레포지토리 메소드에 쿼리 정의
    2. @Query -> 값, DTO 조회

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
 import java.util.List;
@@ -17,8 +18,21 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     //getOne(ID) : 엔티티를 프록시로 조회, 내부에서 EntityManager.getReference()호출
     //findAll() : 모든 엔티티를 조회, 정렬(Sort)이나 페이징(Pageable)조건을 파라미터로 제공
 
+    //메소드 이름으로 쿼리 생성
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 
+    //JPA NamedQuery
     //@Query(name = "Member.findByUsername")
     List<Member> findByUsername(@Param("username") String username);
+
+    //@Query -> 레포지토리 메소드에 쿼리 정의하기
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    //@Query -> 값,DTO 조회하기
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();
+
+    @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
 }
