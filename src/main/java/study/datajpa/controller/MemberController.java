c52e36a77335b164a9dc3136d6d498fac4c5439a
taz-dev <tazdev@naver.com>
2021-11-09 20:20:40 +0900

    feat: 확장 기능
    Web 확장 - 페이징과 정렬

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -1,9 +1,13 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -27,8 +31,22 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5) Pageable pageable) {
+        //Page<Member> page = memberRepository.findAll(pageable); //엔티티를 외부로 노출해서는 안됨!!
+        //Page<MemberDto> map = page.map(member -> new MemberDto(member.getId(), member.getUsername(), null));
+        //return map; --> Ctrl + Alt + N
+        return memberRepository.findAll(pageable)
+                //.map(member -> new MemberDto(member.getId(), member.getUsername(), null));
+                //.map(member -> new MemberDto(member));
+                .map(MemberDto::new);
+    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+        //memberRepository.save(new Member("userA"));
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
     }
 }
